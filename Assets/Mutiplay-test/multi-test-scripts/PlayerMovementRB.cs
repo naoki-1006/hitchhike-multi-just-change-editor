43257c012d3ec960b987e6bd9eeecddacd92a9c7
Naoki Fujimoto <nao-nao-7070@g.ecc.u-tokyo.ac.jp>
2025-06-30 03:04:09 +0900

    add online sphere to NGO_setup

--- Assets/Mutiplay-test/multi-test-scripts/PlayerMovementRB.cs
+++ Assets/Mutiplay-test/multi-test-scripts/PlayerMovementRB.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using Unity.Netcode;
+#if NEW_INPUT_SYSTEM_INSTALLED
+using UnityEngine.InputSystem;
+#endif
 
-public class PlayerMovementRB : MonoBehaviour
+
+public class PlayerMovementRB : NetworkBehaviour
 {
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 500.0f;
     private Rigidbody rb;
 
     void Start()
@@ -12,11 +17,12 @@ public class PlayerMovementRB : MonoBehaviour
 
     void FixedUpdate() // 物理演算はFixedUpdateで行うのが推奨
     {
+        if (!IsOwner || !IsSpawned) return;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
         // Rigidbodyを使って移動
-        rb.linearVelocity = new Vector3(movement.x * moveSpeed, rb.linearVelocity.y, movement.z * moveSpeed);
+        rb.linearVelocity = new Vector3(movement.x * moveSpeed * Time.deltaTime, rb.linearVelocity.y, movement.z * moveSpeed * Time.deltaTime);
     }
 }
