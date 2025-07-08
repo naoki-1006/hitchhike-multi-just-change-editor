b37f5a34b15d8200cfe2514d902dc6b351f65dd9
Naoki Fujimoto <nappnapp7070@gmail.com>
2025-07-08 16:54:05 +0900

    successfully implement hand override by gaze

--- Assets/Mutiplay-test/multi-test-scripts/OwnershipHandler.cs
+++ Assets/Mutiplay-test/multi-test-scripts/OwnershipHandler.cs
@@ -23,7 +23,7 @@ public class OwnershipHandler : NetworkBehaviour
     /// </summary>
     public void ReleaseOwnership()
     {
-        //if (!IsOwner) return;
+        if (!IsOwner) return;
         ReleaseOwnershipServerRpc();
     }
 
