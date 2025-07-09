6fd5f95c31938b8886a1056bdbc9d9c0973c55ae
Naoki Fujimoto <nao-nao-7070@g.ecc.u-tokyo.ac.jp>
2025-07-09 14:33:52 +0900

    stop owner ubau

--- Assets/Mutiplay-test/multi-test-scripts/OwnershipRequester.cs
+++ Assets/Mutiplay-test/multi-test-scripts/OwnershipRequester.cs
@@ -3,56 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace HitchHikeMultiplayer{
-public class OwnershipRequester : MonoBehaviour
-{
-    // シーン内の全ての共有ハンドのOwnershipHandlerを保持するリスト
-    private List<OwnershipHandler> _allHandlers = new List<OwnershipHandler>();
-
-    void Start()
+    public class OwnershipRequester : MonoBehaviour
     {
-        // シーンが読み込まれた際に、一度だけ全てのOwnershipHandlerを探してリストに格納する
-        _allHandlers = FindObjectsOfType<OwnershipHandler>().ToList();
+        // シーン内の全ての共有ハンドのOwnershipHandlerを保持するリスト
+        private List<OwnershipHandler> _allHandlers = new List<OwnershipHandler>();
 
-        if (_allHandlers.Count == 0)
+        void Start()
         {
-            Debug.LogWarning("シーンにOwnershipHandlerを持つオブジェクトが見つかりませんでした。");
-        }
-    }
+            // シーンが読み込まれた際に、一度だけ全てのOwnershipHandlerを探してリストに格納する
+            _allHandlers = FindObjectsOfType<OwnershipHandler>().ToList();
 
-    void Update()
-    {
-        // 数字キーの入力をチェック
-        if (Input.GetKeyDown(KeyCode.Alpha1)) { SetExclusiveOwnership("1"); }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) { SetExclusiveOwnership("2"); }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) { SetExclusiveOwnership("3"); }
-        if (Input.GetKeyDown(KeyCode.Alpha4)) { SetExclusiveOwnership("4"); }
-    }
-
-    /// <summary>
-    /// 指定された識別子を持つハンドの所有権を要求し、それ以外の所有権を放棄する
-    /// </summary>
-    /// <param name="targetIdentifier">オブジェクト名に含まれる数字（"1", "2" など）</param>
-    private void SetExclusiveOwnership(string targetIdentifier)
-    {
-        Debug.Log($"'{targetIdentifier}' を含むハンドの所有権を要求します。");
-
-        foreach (var handler in _allHandlers)
-        {
-            // オブジェクト名にターゲットの数字が含まれているかチェック
-            if (handler.gameObject.name.Contains(targetIdentifier))
+            if (_allHandlers.Count == 0)
             {
-                // これがターゲットなので、所有権を要求
-                handler.RequestOwnership();
-            }
-            else
-            {
-                // これ以外のものなので、所有権を放棄（もし自分がオーナーなら）
-                handler.ReleaseOwnership();
+                Debug.LogWarning("シーンにOwnershipHandlerを持つオブジェクトが見つかりませんでした。");
             }
         }
+
+        void Update()
+        {
+            // 数字キーの入力をチェック
+            if (Input.GetKeyDown(KeyCode.Alpha1)) { SetExclusiveOwnership("1"); }
+            if (Input.GetKeyDown(KeyCode.Alpha2)) { SetExclusiveOwnership("2"); }
+            if (Input.GetKeyDown(KeyCode.Alpha3)) { SetExclusiveOwnership("3"); }
+            if (Input.GetKeyDown(KeyCode.Alpha4)) { SetExclusiveOwnership("4"); }
+        }
+
+        /// <summary>
+        /// 指定された識別子を持つハンドの所有権を要求し、それ以外の所有権を放棄する
+        /// </summary>
+        /// <param name="targetIdentifier">オブジェクト名に含まれる数字（"1", "2" など）</param>
+        private void SetExclusiveOwnership(string targetIdentifier)
+        {
+            Debug.Log($"'{targetIdentifier}' を含むハンドの所有権を要求します。");
+            if (ChangeOwnerChecker(targetIdentifier))
+            {
+
+                foreach (var handler in _allHandlers)
+                {
+                    // オブジェクト名にターゲットの数字が含まれているかチェック
+                    if (handler.gameObject.name.Contains(targetIdentifier))
+                    {
+                        // これがターゲットなので、所有権を要求
+                        handler.RequestOwnership();
+                    }
+                    else
+                    {
+                        // これ以外のものなので、所有権を放棄（もし自分がオーナーなら）
+                        handler.ReleaseOwnership();
+                    }
+                }
+            }
+        }
+        public void SetExclusiveOwnership()
+        {
+            SetExclusiveOwnership(name);
+        }
+        private bool ChangeOwnerChecker(string targetIdentifier)
+        {
+            foreach (var handler in _allHandlers)
+            {
+                // オブジェクト名にターゲットの数字が含まれているかチェック
+                if (handler.gameObject.name.Contains(targetIdentifier))
+                {
+                    return handler.OwnerId == -1;
+                }
+
+            }
+            return false;
+        }
     }
-    public void SetExclusiveOwnership(){
-        SetExclusiveOwnership(name);
-    }
-}
 }
